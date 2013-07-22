4f4585de976d41c21cb783ab3bdcaee5d4a05cc3
Raam Dev <raam@raamdev.com>
2013-07-22 12:13:55 -0400

    * Update example config file to use Penny Arcade Mailer defaults

--- config.example.php
+++ config.example.php
@@ -1,15 +1,15 @@
 <?php if ( empty($here) ) exit('No direct script access allowed');
 
 /**
- * xkcd-Mailer configuration example
+ * PennyArcade-Mailer configuration example
  * Save me as config.php
  */
 
 // Your timezone, PHP5 required.
 // See full list: http://www.php.net/manual/en/timezones.php
-date_default_timezone_set("Europe/Helsinki");
+date_default_timezone_set("America/New_York");
 
 // Your destination
 $mail = "your@email.com";
-$from = "xkcd mailer <xkcdmailer@example.com>";
+$from = "Penny Arcade Mailer <penny.arcade.mailer@example.com>";
 
